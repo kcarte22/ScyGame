f6c4f5c10d19ee864764adad128d9718857472ec
Kaylin Carter <kcarte22@mail.umw.edu>
2021-12-02 22:47:56 -0500

    Work on itemgenerator and weapon types

--- ItemGenerator.java
+++ ItemGenerator.java
@@ -5,7 +5,10 @@ import java.util.Random;
 import java.util.Collections;
 import java.util.Arrays;
 import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ItemGenerator {
 
@@ -22,7 +25,19 @@ public class ItemGenerator {
         return new Item(ItemType.Other, "Vase", 10, 2, 0);
     }
 
-    public static ArrayList<String> loadWeaponOptions(ArrayList<String> weapons) {
+    public static ArrayList<String> loadWeaponOptions(Scanner s, ArrayList<String> weapons) throws FileNotFoundException {
+            s = new Scanner(new File("weapons.txt"));
+            String weapon;
+            for (int i = 0; i < weapons.size(); i++) {
+                weapon = s.next();
+                weapons.add(weapon);
+                if (weapon == null) {
+                    break;
+                }
+            }
+
+
+
         return null;
     }
 }
